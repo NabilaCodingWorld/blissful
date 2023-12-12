418a69348bdb29493d74a0063235d20b4cc8ee0e
NabilaWorld <mnabila30163@gmail.com>
2023-12-12 18:59:26 +0600

    commit gallery

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Features from "@/components/Features";
+import Gallery from "@/components/Gallery";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
 import Products from "@/components/Products";
@@ -12,6 +13,7 @@ const HomePage = () => {
       <Services />
       <Pricing />
       <Products />
+      <Gallery />
     </main>
   );
 };
